a620eb0e55b34194ac300bb7a9f9bf586446509d
Божок Пирожок <iljaturbine@gmail.com>
2024-09-22 21:24:03 +0300

    Исполняемый файл, Test

---
+++ Labs/Lab-1/Lab-1/test.cpp
@@ -0,0 +1,18 @@
+#include "test.h"
+/// <summary>
+/// ѕустой  онструктор
+/// </summary>
+test::test()
+{
+}
+
+/// <summary>
+/// ‘унки€ по поиску минимального
+/// </summary>
+/// <param name="a">первое число, которое нужно сравнивать</param>
+/// <param name="b">второе число которое нужно сравнивать</param>
+/// <returns></returns>
+int test::min(int a, int b)
+{
+    return ((a < b) ? (a) : (b));
+}
