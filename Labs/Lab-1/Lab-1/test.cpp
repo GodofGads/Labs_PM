58df02a43bc7a7c71de8cdbc2f2b8b9d06be0ad4
Иван Васильев <vanyshka.vasilek@outlook.com>
2024-09-22 22:39:40 +0300

    Поправил класс

--- Labs/Lab-1/Lab-1/test.cpp
+++ Labs/Lab-1/Lab-1/test.cpp
@@ -24,10 +24,12 @@ int test::min(std::vector<int> vec)
     return fule;
 }
 
-int sheet() {
+int sheet()
+{
     int array_of_int[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
 
     for (int i = 0; i < 10; i++) {
         array_of_int[i] = i * 100;
         cout << array_of_int[i] << endl;
     }
+}
