4247bf80a1ba073cbf85181945a2772ca32c88b6
Иван Васильев <vanyshka.vasilek@outlook.com>
2024-09-22 22:30:28 +0300

    Сделал поправку класса "Vector", поменял ее расширение на int, а так же сделал объявление в .cpp файле

--- Labs/Lab-1/Lab-1/test.cpp
+++ Labs/Lab-1/Lab-1/test.cpp
@@ -1,4 +1,6 @@
 #include "test.h"
+#include <iostream>
+using namespace std;
 /// <summary>
 /// Пустой Конструктор
 /// </summary>
@@ -16,3 +18,16 @@ int test::min(int a, int b)
 {
     return ((a < b) ? (a) : (b));
 }
+
+int test::min(std::vector<int> vec)
+{
+    return fule;
+}
+
+int sheet() {
+    int array_of_int[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+    for (int i = 0; i < 10; i++) {
+        array_of_int[i] = i * 100;
+        cout << array_of_int[i] << endl;
+    }
